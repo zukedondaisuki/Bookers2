c42cb37f0abbc47081e1ef5edf5d632935ef0541
zukedondaisuki <a.happy.coaster@outlook.jp>
2021-05-02 05:56:43 +0000

    [Update]いいね機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_024620) do
+ActiveRecord::Schema.define(version: 2021_05_02_044703) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
