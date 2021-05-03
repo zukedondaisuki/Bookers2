bd4ed890834c0eca78d9b63a9ddd5717e5a88f86
zukedondaisuki <a.happy.coaster@outlook.jp>
2021-05-03 12:09:46 +0000

    [Update]ルーティング＆検索フォーム生成

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
+
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
   end
+
   resources :users, only: [:index, :show, :edit, :update]
+
+  get "search" => "searches#search"
 
   resources :users do
     member do
